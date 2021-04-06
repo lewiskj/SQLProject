236e5c5ec38e3534da89d82a5a12bd60f0ae2de5
Kyle Lewis <kylelewis57@hotmail.com>
2021-04-06 17:26:32 -0400

    Add files via upload
    FOR THE THIRD TIME, COMMIT

--- start.py
+++ start.py
@@ -1,18 +1,44 @@
 from tkinter import *
+from tkcalendar import *
+
+Vs = str("     ")                                                       #Frequently Used: 5 Spaces as a string
+defFont = ("Arial", "22")                                               #Frequently Used: Default Font
+
 root = Tk()
 root.minsize(640,480)
-root.maxsize(1280,1024) #Maximum size not explicitly needed
+root.maxsize(1280,1024)                                                 #Maximum size not explicitly needed
 
-#module 1
-var = StringVar()                                                               #Sets internal variable for this module, a label, as a string.
-titleLabel = Label(root, textvariable=var, relief=FLAT, font=("Arial", "22"))   #specs for said string
-var.set("Reservation System Manager v.A")                                       #the text itself
-titleLabel.grid(row=0,column=0,sticky="w")                                                      #I believe this prints it to the window
+#module 1 - Title
+title = Label(text="Reservation System Manager v.A", font=(defFont))    #specs for said string
+title.grid(row=0,column=1,sticky="w")                                   #positional arguments; "w" means west; NSEW and can use NW & SE
 
-#module 2
-L1 = Label(text="\n"*2 + "User Name" + " "*5)
-L1.grid(row=2, column=0, sticky="w")
-E1 = Entry(bd=6)
-E1.grid(row=3, column=0, sticky="w")
+#module 2 - First Name Entry
+FN = Label(text="First Name:" + Vs, font=(defFont))                     #~: Sets up the actual text
+FN.grid(row=2, column=1, sticky="w")                                    #~~: positional arguments
+
+FN_entry = Entry(bd=6, width=25)                                        #~~~: Creates the entry box next to it
+FN_entry.grid(row=2, column=2, sticky="w")                              #~~~~: positional arguments; ".grid()" always indicates this.
+
+#module 3 - Last Name Entry
+LN = Label(text="Last Name: ", font =(defFont))
+LN.grid(row=3, column=1, sticky="w")
+
+LN_entry = Entry(bd=6, width=25)
+LN_entry.grid(row=3, column=2, sticky="w")
+
+#module 4 - Starting Date Entry
+SD = Label(text="Starting Date:" + Vs,font=(defFont))
+SD.grid(row=4, column=1, sticky="w")
+
+SD_calendar = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)   #visual arguments
+SD_calendar.grid(row=4, column=2, sticky="w")
+
+#module 5 - Ending Date Entry
+ED = Label(text="Ending Date:" + Vs, font=(defFont))
+ED.grid(row=5, column=1, sticky="w")
+
+ED_calendar = DateEntry(root, width=12, background='darkblue', foreground='white', borderwidth=2)   #visual arguments
+ED_calendar.grid(row=5, column=2, sticky="w")
+
 
 root.mainloop() #Calls the mainloop of TK, per the description
